3c066544bfce9c871cb3c90416cd5d46fb8c3531
Paul Rentschler <par117@psu.edu>
2022-10-31 11:28:02 -0400

    Custom versions of id, title, and description

--- src/plonetheme/eeq/content/person.py
+++ src/plonetheme/eeq/content/person.py
@@ -13,3 +13,38 @@ alsoProvides(IPerson, IContentType)
 
 class Person(Item):
     portal_type = 'plonetheme.eeq.person'
+
+    @property
+    def description(self):
+        job_titles = self.jobtitles.replace('\r\n', '\n').split('\n')
+        try:
+            return job_titles[0]
+        except IndexError:
+            return ''
+
+    @property
+    def display_name(self):
+        name = '{} {}'.format(self.first_name, self.last_name)
+        if self.suffix:
+            name += ', {}'.format(self.suffix)
+        return name
+
+    @property
+    def email(self):
+        if self.alternate_email:
+            return self.alternate_email
+        return '{}@psu.edu'.format(self.id)
+
+    @property
+    def office_phone(self):
+        phone = (self.office_phone_number.replace('-', '')
+                                         .replace('(', '')
+                                         .replace(')', '')
+                                         .replace(' ', ''))
+        if len(phone) != 10:
+            return self.office_phone_number
+        return '{}-{}-{}'.format(phone[:3], phone[3:3], phone[6:])
+
+    @property
+    def title(self):
+        return '{}, {}'.format(self.last_name, self.first_name)
