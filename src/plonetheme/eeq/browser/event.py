1605a0f724b232632d4519e392fe068fd271f69c
Paul Rentschler <par117@psu.edu>
2023-11-15 09:32:38 -0500

    Event Link objects have an URL not a  location

--- src/plonetheme/eeq/browser/event.py
+++ src/plonetheme/eeq/browser/event.py
@@ -4,27 +4,34 @@ from Products.Five.browser import BrowserView
 
 
 class PSUEventHelpers(BrowserView):
-    """Helper functions for event content type.
-    """
+    """Helper functions for event content type"""
+
     def gcal_url(self):
-        """Returns a Google Calendar URL for the context event.
-        """
+        """Returns a Google Calendar URL for the context event"""
         dates = "/".join((
             self.context.start.strftime('%Y%m%dT%H%S%M'),
             self.context.end.strftime('%Y%m%dT%H%S%M')
         ))
+        try:
+            event_location = self.context.location
+        except AttributeError:
+            try:
+                event_location = self.context.remoteUrl
+            except AttributeError:
+                event_location = ""
         return "https://calendar.google.com/calendar/event?" + urlencode({
                 "action": "TEMPLATE",
                 "text": self.context.title,
                 "sprop": self.context.absolute_url(),
                 "details": self.context.description,
                 "dates": dates,
-                "location": self.context.location or "",
+                "location": event_location,
             })
 
     def format_time(self, time):
-        """Format the given `time` (a datetime object) like this:
-        '7:00 p.m.' / '11:00 a.m.' / '9:00 a.m.'
+        """Format the given `time` (a datetime object)
+
+        Examples: '7:00 p.m.' / '11:00 a.m.' / '9:00 a.m.' / 'All day'
         """
         if self.context.whole_day:
             return 'All day'
